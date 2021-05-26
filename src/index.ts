cf080d5e7dfd91c55659b859dd7091c414df1a7a
Restyled.io <commits@restyled.io>
2021-05-26 11:20:14 +0000

    Restyled by whitespace

--- src/index.ts
+++ src/index.ts
@@ -73,7 +73,7 @@ async function exec_role(msg: Discord.Message, arg: string) {
 
     arg = arg.toLowerCase();
     let role_data = roles.find(c => c.id === arg);
-    
+
     if(!role_data){
         if(arg[0]=='y') return msg.channel.send("Are you from IITK? If yes then reach out to the moderators to get your batch role added to the list.");
         else return msg.channel.send(`Hey first take a look at the available roles.`);
@@ -85,7 +85,7 @@ async function exec_role(msg: Discord.Message, arg: string) {
 
     //msg.channel.send(`Giving ${member.displayName} the role ${arg}`);
 
-        
+
 
     if (!member) {
         return msg.channel.send("Can't find the user in server");
@@ -123,8 +123,8 @@ async function exec_role(msg: Discord.Message, arg: string) {
         }
     }
 
-    
-    
+
+
 }
 
 // Register callbacks
@@ -203,7 +203,7 @@ client.on('message', async function (msg: Discord.Message) {     //: Discord.Mes
         */
 // TILL HERE -------------------------------------------------------------------------------------------------------------------------//
 
-        
+
     }
 
 });
@@ -213,4 +213,3 @@ client.on('message', async function (msg: Discord.Message) {     //: Discord.Mes
 
 // You'll need to keep the bot token as a Heroku env var
 client.login(process.env["BOT_TOKEN"]);
-
